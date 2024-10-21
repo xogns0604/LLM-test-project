769b1a220b2bdb44efaa6129b2d6511647edc11a
thlee <xogns0604@naver.com>
2024-10-22 08:33:58 +0900

    thlee
    초기구성

---
+++ src/main/java/com/LLM_test_project/LlmTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.LLM_test_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmTestProjectApplication.class, args);
+	}
+
+}
